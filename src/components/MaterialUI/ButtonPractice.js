3b63b56d843f035ff3e76e27e6df8239aba2d3c8
sakhawat777 <biplobbms@gmail.com>
2021-11-10 21:09:23 +0600

    Button Practice Done

--- src/components/MaterialUI/ButtonPractice.js
+++ src/components/MaterialUI/ButtonPractice.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import '../MaterialUI/ButtonPractice.css';
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
 const ButtonPractice = () => {
 	return (
 		<div>
-			<Button variant='contained'>Hello World</Button>
+			<Button variant='contained' className='single-btn'>
+				Hello Material UI
+			</Button>{' '}
+			<br />
+			<ButtonGroup
+				variant='contained'
+				aria-label='outlined primary button group'>
+				<Button>Previous</Button>
+				<Button>Pause</Button>
+				<Button>Next</Button>
+			</ButtonGroup>
 		</div>
 	);
 };
