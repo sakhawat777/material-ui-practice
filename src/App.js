26075bfda33e161b4308ffa165e51691cd706d7a
sakhawat777 <biplobbms@gmail.com>
2021-11-10 20:52:07 +0600

    Practice Start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ButtonPractice from './components/MaterialUI/ButtonPractice';
+
+function App() {
+	return (
+		<div>
+			<h1>Material UI</h1>
+			<ButtonPractice></ButtonPractice>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/MaterialUI/ButtonPractice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+const ButtonPractice = () => {
+	return (
+		<div>
+			<Button variant='contained'>Hello World</Button>
+		</div>
+	);
+};
+
+export default ButtonPractice;
